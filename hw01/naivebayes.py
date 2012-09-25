8b369394d8419b9e68427fad17e331c16dfb668d
Willie Neiswanger <willie@host-128-237-200-110.wv.cc.cmu.edu>
2012-09-25 00:38:32 -0400

    added .gitignore file and rearranged some stuff

---
+++ hw01/naivebayes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+naive bayes implementation for ml701 class.
+willie neiswanger, 09/2012.
+"""
+
+# parse data
+
+# split data into training/test
+
+# take subset of training data
+
+# carry out naive bayes on training data
+
+# test model on testing data
+
+
+
+# main function (carries out naive bayes part of 3.4 of hw)
+# parse data, for 100 times: split data (into train/test), for train-size ranging from 2:max-size (2:200?), learn model, test on test set [and compute / record accuracy]. afterwards, make plot.
