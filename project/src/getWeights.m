ca2a3f5eb116dcf4912f7b68c672845099a09c6f
willieneis <willie.neiswanger@gmail.com>
2012-11-04 02:48:55 -0500

    added catrnd.m function to project/src/

--- project/src/getWeights.m
+++ project/src/getWeights.m
@@ -1,20 +1,17 @@
 function weights = getWeights(samples,data)
 
-% getWeights(A,b,D) Compute importance weights for each sample (for smcSimple)
+% getWeights(samples,data) Compute importance weights for each sample (for smcSimple)
 %
 % Description of function
 %
 % Inputs:
-% (1) Input one
-% (2) Input two
-% (3) Input three
+% (1) samples are 
+% (2) data from a single time step (matrix of size n x d)
 
+%%%% input samples could be: only the newest precision matrices, or newest precision matrices and graphs, or all precision matrices and graphs.
 
-% Notes:
-% input samples could be: only the newest precision matrices, or newest precision matrices and graphs, or all precision matrices and graphs.
-% data could be: entire data cell or just data for relevant time step
-
-% instead of likelihood (product) of prob of each data point, could take sum of log prob of each data point.
-
-
-weight = mvnpdf(data,0*
+d = size(data,2); % dimension of data / MVN params 
+Kvec = %%%% a cell of precision-matrix-samples, gotten from samples input
+for k=1:length(Kvec)
+    weights(k) = sum(my_log_mvnpdf_prec(data,zeros(1,d),Kvec{k}));
+end
