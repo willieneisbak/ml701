6383b55e4f523181215f5c13f1ea2149359f4de1
willieneis <willie.neiswanger@gmail.com>
2012-11-04 23:50:25 -0500

    added project/src/expectedGraph.m file

---
+++ project/src/expectedGraph.m
@@ -0,0 +1,15 @@
+function meanGraph = expectedGraph(graphCell,weightVec)
+
+% this function returns the expected graph given a cell of graph samples
+% and a vector of weights-per-sample
+%
+% Inputs:
+% (1) graphCell is a 1xP cell-array of adjacency matrices 
+% (2) weightVec is a 1xP vector of weights-per-sample
+
+sumGraphs = zeros(length(graphCell{1})); % init sumGraphs to all zeros
+for i=1:length(graphCell)
+    sumGraphs = sumGraphs + (graphCell{i}/weightVec(i)); % add each graph, weighted, onto sumGraphs
+end
+denom = sum(ones(1,length(graphCell))./weightVec); % compute denominator
+meanGraph = sumGraphs / denom;
